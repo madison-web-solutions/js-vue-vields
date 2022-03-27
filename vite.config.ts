e7b29e977b15d0535b840daf20fa146ed4faed06
Dan Howard <dh@madisonsolutions.co.uk>
2022-03-27 16:19:40 +0100

    Add date-format-ms external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -21,14 +21,7 @@ module.exports = defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
+      external: ['vue', 'date-format-ms'],
     }
   }
 });
