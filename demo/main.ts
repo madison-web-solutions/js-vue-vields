f26a0d7cfbb7c484354d3ad65bcd08514ce2d2bb
Dan Howard <dh@madisonsolutions.co.uk>
2023-03-02 20:50:09 +0000

    Fix some nonsense about how to import css in the ddemo

--- demo/main.ts
+++ demo/main.ts
@@ -2,12 +2,7 @@ import { createApp } from "vue";
 import App from './App.vue';
 import { configureApp } from "@/main";
 
-import css from './scss/app.scss';
-// I don't know what's going on here, but if I just import the css module, and don't
-// 'use' it anywhere in the script, the page renders without any styles.
-// Just accessing the variable, as below, seems to make it work... no idea why
-// In the project built from the Vite init script, I don't seem to need this.
-css;
+import "./scss/app.scss";
 
 const app = createApp(App);
 configureApp(app, {
